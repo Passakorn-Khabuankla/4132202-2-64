60cd1b79bdce11279cea807ccc0a96256320f0fe
Passakorn-Khabuankla <630112418017@bru.ac.th>
2021-12-18 12:01:57 +0700

    LAB3

---
+++ lab3/src/components/Dom.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const  age = 20;
+
+const dom = (  
+<>
+<h1>Hello React !!!</h1>
+<p>My name is Passakorn , {age} years old.</p>
+</>
+);
+
+function Dom() {
+    return dom;
+}
+
+export default Dom
